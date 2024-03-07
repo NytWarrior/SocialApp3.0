7283f76a9169166d2f38afdc6b6a923aebf88999
Rajeev <rajeevkr8917@gmail.com>
2024-03-07 17:48:48 +0530

    user models/routes added

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = async (req, res) => {
+    console.log("Signup");
+};
+
+
+export const login = (req, res) => {
+    console.log("Login");
+};
+
+export const logout = (req, res) => {
+    console.log("Logout");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());    //to parse the incoming request with JSON payload(from req.body)
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!!");
