3dfea6fbceaeec42bfcee992bca791dd910ac54e
Rajeev <rajeevkr8917@gmail.com>
2024-03-11 02:41:15 +0530

    signup/login page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import Footer from './components/navbarFooter/Footer'
 import Navbar from './components/navbarFooter/Navbar'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/Signup';
+
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Home />
-      <Footer />
-
+      <div className='p-4 h-screen flex items-center justify-center'>
+        <Login />
+        {/* <Home />
+        <SignUp /> */}
+        <Footer />
+      </div>
     </>
   )
 }
