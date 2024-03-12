219d57d03681bdb03c6a93b5c976ae2932f1ef16
Rajeev <rajeevkr8917@gmail.com>
2024-03-12 16:16:19 +0530

    login/logout and signup added

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import GenderCheckbox from "./GenderCheckbox";
+import useSignup from "../../hooks/useSignup";
 
 
 const SignUp = () => {
@@ -12,16 +14,21 @@ const SignUp = () => {
         gender: "",
     });
 
+    const { loading, signup } = useSignup();
+
+    const handleCheckboxChange = (gender) => {
+        setInputs({ ...inputs, gender });
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(inputs);
-
+        // console.log(inputs);
+        await signup(inputs);
     };
 
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-            <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20'>
+            <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
                 <h1 className='text-3xl font-semibold text-center text-gray-300'>
                     Sign Up <span className='text-blue-500'> SocialApp</span>
                 </h1>
@@ -73,13 +80,15 @@ const SignUp = () => {
                         />
                     </div>
 
+                    <GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
+
                     <Link to={"/login"} className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
                         Already have an account?
                     </Link>
 
                     <div>
-                        <button className='btn btn-block btn-sm mt-2 border border-slate-700'>
-                            SignUp
+                        <button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+                            {loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
                         </button>
                     </div>
                 </form>
