c108977746f844a8e070285cf79273b151b85eba
Rajeev <rajeevkr8917@gmail.com>
2024-03-12 17:18:29 +0530

    protectRoute added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createPost, destroyPost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/create", createPost);
-router.get("/", getPosts);
-router.get("/post/:id", getPost);
-router.put("/update/:id", updatePost);
-router.delete("/destroy/:id", destroyPost);
+router.post("/create", protectRoute, createPost);
+router.get("/", protectRoute, getPosts);
+router.get("/post/:id", protectRoute, getPost);
+router.put("/update/:id", protectRoute, updatePost);
+router.delete("/destroy/:id", protectRoute, destroyPost);
 
 export default router;
